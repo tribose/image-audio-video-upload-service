86f3605cf29e856573adddef2e875958fcea02bd
tbose4 <tridib.bose1@metlife.com>
2020-08-01 11:47:57 +0530

    first commit

---
+++ src/main/java/com/blobImage/imageaudiovideouploadservice/ImageAudioVideoUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blobImage.imageaudiovideouploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAudioVideoUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageAudioVideoUploadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blobImage/imageaudiovideouploadservice/ImageAudioVideoUploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blobImage.imageaudiovideouploadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAudioVideoUploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
